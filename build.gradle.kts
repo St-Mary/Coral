b26e629b645494e33b278eaa417fb377b8eb5a25
Noelle-Ai <gerardelliot49@gmail.com>
2023-12-23 08:23:12 +0100

    v1.0.8 (little fix)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/St-Mary/CommonLib")
+            url = uri("https://maven.pkg.github.com/St-Mary/Coral")
             credentials {
                 username = (System.getenv("GITHUB_ACTOR") ?: "").toString()
                 password = (System.getenv("GITHUB_TOKEN") ?: "").toString()
